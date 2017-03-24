47650baa65ff9934acc5b54c864e32fa6ce8922a
Christelle ROUSSEL <croussel@adneom.com>
2017-03-24 09:22:09 +0100

    set up 3 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LeftComponent } from './left/left.component';
+import { MiddleComponent } from './middle/middle.component';
+import { RightComponent } from './right/right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftComponent,
+    MiddleComponent,
+    RightComponent
   ],
   imports: [
     BrowserModule,
